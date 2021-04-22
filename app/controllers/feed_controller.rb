04d8648d522ffb3f9eb2663bf2e5caa25b2ab45d
Fabian Conejo <fabianconejosegu@gmail.com>
2021-04-22 17:07:18 -0600

    Pending bug fix for friendship relations

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,4 +1,5 @@
 class FeedController < ApplicationController
     def index
+        @users = User.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :users do 
+    resources :relationships, only: %i[ create ]
+  end
+
+  delete 'relationships/destroy'
+
   root 'feed#index'
+
 end
