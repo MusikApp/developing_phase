63368fdc406413d70ed5dd69a3799f6bf5988017
Fabian Conejo <fabianconejosegu@gmail.com>
2021-04-22 15:42:45 -0600

    Add User model via devise and avatar attachments option.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    def configure_permitted_parameters               
+        devise_parameter_sanitizer.permit(:account_update) do |user| 
+        user.permit(:avatar, :username, :name, :last_name, :email, :password, :password_confirmation, :current_password) 
+        end
+    end
+end
