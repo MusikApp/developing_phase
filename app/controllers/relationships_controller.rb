fae0e92375f896475d048288e7708a662062c951
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-04-22 19:30:58 -0600

    Follower button and relationships

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,23 @@
 class RelationshipsController < ApplicationController
+    before_action :authenticate_user!
+
+
     def create
         other_user = User.find(params[:user_id])
         @rel = Relationship.new(follower_id: current_user.id,
                                  followed_id: other_user.id)
         @rel.save
-        redirect_to user_path(other_user)
+        # redirect_to user_path(other_user)
+        redirect_to root_path
+
+  
     end
     
     def destroy
         @rel = Relationship.find(params[:id])
         @rel.destroy
-        redirect_to user_path(@rel.followed_id)
+        # redirect_to user_path(@rel.followed_id)
+        redirect_to root_path
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users do 
-    resources :relationships, only: %i[ create ]
+    resources :relationships, only: [:create]
   end
-
-  delete 'relationships/destroy'
+  
+  resources :relationships, only: [:destroy]
 
   root 'feed#index'
 
