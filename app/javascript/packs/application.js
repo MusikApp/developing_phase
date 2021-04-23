fac2d84fa605c655ed497e0208c6d33d78967315
Fabian Conejo <fabianconejosegu@gmail.com>
2021-04-23 11:46:35 -0600

    Add buttons to navbar, and an animated menu with jquery.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import jQuery from "jquery"
+
+global.$ = global.jQuery = jQuery
+window.$ = window.jQuery = jQuery
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', () => {
+    $(".navbar-thumbnail").on("click", () => {
+        $(".menu").toggle();
+        $(".menu-list").on("click", () => {
+            $(".menu").hide();
+        })
+    })
+})
