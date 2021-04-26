2f4ec4fa4ce8309760c615bee408c0a9336a3fdf
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-04-26 15:29:37 -0600

    Like Function added

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,49 @@
+class LikesController < ApplicationController
+  before_action :set_like, only: %i[ destroy ]
+  before_action :find_post
+  before_action :find_like, only: [:destroy]
+
+  def index
+  end
+
+  def create
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+    @findpost.likes.create(user_id: current_user.id)
+    redirect_to root_path
+    end
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @findlike.destroy
+    end
+    redirect_to root_path
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_like
+    @like = Like.find(params[:id])
+  end
+
+  def find_post
+    @findpost = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
+  end
+
+  def find_like
+    @findlike = @findpost.likes.find(params[:id])
+ end
+  # Only allow a list of trusted parameters through.
+  def like_params
+    params.require(:like).permit(:id, :user_id, :post_id)
+  end
+end
