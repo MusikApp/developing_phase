f9bd37247056bd7971bd32fe7b9c273ab0c98cd4
Randall Chaves Camacho <rchavesc1@gmail.com>
2021-05-12 04:10:39 -0600

    commented app/notifications/comment_notification.rb as heroku rails console mentioned it to be uninitialized constant CommentNotification

--- app/notifications/comment_notificaction.rb
+++ app/notifications/comment_notificaction.rb
@@ -1,30 +1,30 @@
 # To deliver this notification:
 #
-CommentNotificaction.with(comment: @comment).deliver_later(@comment.post.user)
-# notification = CommentNotificaction.with(comment: @comment)
-# notification.deliver(@comment.post.user)
-#CommentNotificaction.with(post: @post).deliver(@comment.post.user)
+# CommentNotificaction.with(comment: @comment).deliver_later(@comment.post.user)
+# # notification = CommentNotificaction.with(comment: @comment)
+# # notification.deliver(@comment.post.user)
+# #CommentNotificaction.with(post: @post).deliver(@comment.post.user)
 
-class CommentNotificaction < Noticed::Base
-  # Add your delivery methods
-  #
-  deliver_by :database
-  deliver_by :action_cable
-  # deliver_by :email, mailer: "UserMailer"
-  # deliver_by :slack
-  # deliver_by :custom, class: "MyDeliveryMethod"
+# class CommentNotificaction < Noticed::Base
+#   # Add your delivery methods
+#   #
+#   deliver_by :database
+#   deliver_by :action_cable
+#   # deliver_by :email, mailer: "UserMailer"
+#   # deliver_by :slack
+#   # deliver_by :custom, class: "MyDeliveryMethod"
 
-  # Add required params
-  #
-  param :post
+#   # Add required params
+#   #
+#   param :post
 
-  # Define helper methods to make rendering easier.
-  #
-  def message
-    t(".message")
-  end
+#   # Define helper methods to make rendering easier.
+#   #
+#   def message
+#     t(".message")
+#   end
 
-  def url
-    post_path(params[:post])
-  end
-end
+#   def url
+#     post_path(params[:post])
+#   end
+# end
